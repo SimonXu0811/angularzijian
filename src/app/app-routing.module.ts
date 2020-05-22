bd09c5618ac2493100b55e8db1742efe92a0d43e
Xuzijian <zix10@pitt.edu>
2020-05-22 00:07:04 -0400

    start build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AboutpageComponent} from "./aboutpage/aboutpage.component";
+import {HomepageComponent} from "./homepage/homepage.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path:'home', component: HomepageComponent},
+  {path: 'about', component: AboutpageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import {MatSidenavModule} from "@angular/material/sidenav";
+import { CollapseComponent } from './homepage/components/collapse-framework/collapse.component';
+import { CardComponent } from './homepage/components/skill-card/card.component';
+import { AboutpageComponent } from './aboutpage/aboutpage.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CollapseComponent,
+    CardComponent,
+    AboutpageComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
