4c1a150ce957ca340a9fcea56487871795ecab70
Xuzijian <zix10@pitt.edu>
2020-05-22 11:36:18 -0400

    url 404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './navbar/navbar.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NavbarComponent} from './navbar/navbar.component';
 import {MatSidenavModule} from "@angular/material/sidenav";
-import { CollapseComponent } from './homepage/components/collapse-framework/collapse.component';
-import { CardComponent } from './homepage/components/skill-card/card.component';
-import { AboutpageComponent } from './aboutpage/aboutpage.component';
-import { HomepageComponent } from './homepage/homepage.component';
+import {CollapseComponent} from './homepage/components/collapse-framework/collapse.component';
+import {CardComponent} from './homepage/components/skill-card/card.component';
+import {AboutpageComponent} from './aboutpage/aboutpage.component';
+import {HomepageComponent} from './homepage/homepage.component';
 import {MatIconModule} from "@angular/material/icon";
+import {HashLocationStrategy, LocationStrategy} from "@angular/common";
 
 @NgModule({
   declarations: [
@@ -28,7 +29,10 @@ import {MatIconModule} from "@angular/material/icon";
     MatSidenavModule,
     MatIconModule
   ],
-  providers: [],
+  providers: [
+    {provide: LocationStrategy, useClass: HashLocationStrategy}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
